92b34f829ccb3d0e232b80cf9d7b4929accf68b0
obaychu <olivia.baychu@edu.sait.ca>
2024-04-22 12:54:25 -0600

    added second page

---
+++ file.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+
+    accordionItems.forEach(item => {
+        const header = item.querySelector('.accordion-header');
+
+        header.addEventListener('click', () => {
+            item.classList.toggle('active');
+            const content = item.querySelector('.accordion-content');
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+    });
+});
